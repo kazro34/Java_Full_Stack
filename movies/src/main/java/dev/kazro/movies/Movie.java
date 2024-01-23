99e0bd4677b9260eb3900f5eb66cc8213393645b
robert <kasza.robert@stud.uni-obuda.hu>
2024-01-23 10:21:07 +0100

    Project setting up and models

---
+++ movies/src/main/java/dev/kazro/movies/Movie.java
@@ -0,0 +1,31 @@
+package dev.kazro.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String Poster;
+    private List<String> genres;
+    private List<String> backdrops;
+    @DocumentReference //Only IDs will be stored
+    private List<Review> reviewIds;
+
+
+}
