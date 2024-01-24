22e8ebb6615a6b8edc6c2a1860db4708b5233d20
robert <kasza.robert@stud.uni-obuda.hu>
2024-01-24 10:45:42 +0100

    createReview method added

--- movies/src/main/java/dev/kazro/movies/Review.java
+++ movies/src/main/java/dev/kazro/movies/Review.java
@@ -16,4 +16,8 @@ public class Review {
     private ObjectId ID;
 
     private String body;
+
+    public Review(String body) {
+        this.body = body;
+    }
 }
--- movies/src/main/java/dev/kazro/movies/ReviewService.java
+++ movies/src/main/java/dev/kazro/movies/ReviewService.java
@@ -1,7 +1,26 @@
 package dev.kazro.movies;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReviewService {
+    @Autowired
+    private ReviewRepository reviewRepository;
+    @Autowired
+    private MongoTemplate mongoTemplate;
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+        mongoTemplate.update(Movie.class) //We are updating a movie where the IMDB ID of the movie in the database
+                .matching(Criteria.where("imdbId").is(imdbId)) //matches the ID that we have received from the user
+                .apply(new Update().push("reviewIds").value(review))
+                .first();//then we apply this update
+
+        return review;
+
+    }
 }
