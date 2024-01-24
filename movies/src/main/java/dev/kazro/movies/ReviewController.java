574e7d7618b9b585e7d6c8eef23f34438d9a18a2
robert <kasza.robert@stud.uni-obuda.hu>
2024-01-24 19:28:11 +0100

    patch

--- movies/src/main/java/dev/kazro/movies/ReviewController.java
+++ movies/src/main/java/dev/kazro/movies/ReviewController.java
@@ -12,10 +12,11 @@ import java.util.Map;
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
     @Autowired
-    private ReviewService reviewService;
+    private ReviewService service;
 
-    @PostMapping
+    @PostMapping()
     public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload) {
-        return new ResponseEntity<Review>(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.CREATED);
+
+        return new ResponseEntity<Review>(service.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.OK);
     }
 }
