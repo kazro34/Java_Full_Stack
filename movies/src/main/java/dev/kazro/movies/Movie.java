5377cfc392a51cfb950ae425f0950a69d682f83b
robert <kasza.robert@stud.uni-obuda.hu>
2024-01-24 16:04:11 +0100

    Typo fixed

--- movies/src/main/java/dev/kazro/movies/Movie.java
+++ movies/src/main/java/dev/kazro/movies/Movie.java
@@ -21,7 +21,7 @@ public class Movie {
     private String title;
     private String releaseDate;
     private String trailerLink;
-    private String Poster;
+    private String poster;
     private List<String> genres;
     private List<String> backdrops;
     @DocumentReference //Only IDs will be stored
